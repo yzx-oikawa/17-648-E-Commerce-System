7135ea75ef1bcbdd9471db632674d98bf6f87585
yzx-oikawa <zixinye@andrew.cmu.edu>
2020-04-12 22:20:33 -0400

    Move use session to profile.js

--- e-commerce.js
+++ e-commerce.js
@@ -14,15 +14,6 @@ app.use('', profile);
 app.use('', product);
 app.use('', purchase);
 
-// app.use(session({
-//     secret: 'test secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     rolling: true,
-//     cookie: { maxAge: 60 * 1000 * 15 } //ms
-// }));
-
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
